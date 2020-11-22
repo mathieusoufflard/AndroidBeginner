aca86fed859a4df24fb7ea75b2d069d8b50c811c
mathieu soufflard asus <mathieusoufflard@gmail.com>
2020-11-22 17:20:33 +0100

    see how to put an consol log message
    Start the Money project (a simple money convert app).
    	- page layout set

---
+++ TrainnerProject_Money/app/src/main/java/com/mathieu/money/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mathieu.money;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "Hello Android");
+        Log.i("MainActivity", "Hello again Android");
+    }
+}
