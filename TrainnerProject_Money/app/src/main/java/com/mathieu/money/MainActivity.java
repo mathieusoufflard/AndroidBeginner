810b19b70896857acf682ede0ac4d67358c97f13
mathieu soufflard asus <mathieusoufflard@gmail.com>
2021-03-20 17:06:52 +0100

    same, just add some file

--- TrainnerProject_Money/app/src/main/java/com/mathieu/money/MainActivity.java
+++ TrainnerProject_Money/app/src/main/java/com/mathieu/money/MainActivity.java
@@ -2,8 +2,14 @@ package com.mathieu.money;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("MainActivity", "Hello Android");
-        Log.i("MainActivity", "Hello again Android");
+        //Log.i("MainActivity", "Hello Android");
+        //Log.i("MainActivity", "Hello again Android");
+
+        Button convertButton = findViewById(R.id.convertButton);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "convert click");
+                EditText amountEditText = findViewById(R.id.amountEditText);
+                String amountStr = amountEditText.getText().toString();
+                Log.d("MainActivity", "amountStr =" + amountStr);
+
+                if (amountStr.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Montant obligatoire", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                float result = Float.parseFloat(amountStr) * 0.84f;
+                TextView resultText = findViewById(R.id.resultTextView);
+                resultText.setText(result + " $");
+            }
+        });
+
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, aboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
